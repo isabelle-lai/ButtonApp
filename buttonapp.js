73e6f02cd36c819417caabeff43b9d348b6f8894
aidenkang99 <aidentufts22@gmail.com>
2019-11-04 14:14:28 -0500

    Added jQuery

--- buttonapp.js
+++ buttonapp.js
@@ -10,8 +10,12 @@ var width1 = 100;
 var height1 = 100;
 
 function draw(){
-	if (canvas.getContext) {
-		ctx.strokeRect(x1, y1, width1, height1);
-		ctx.strokeRect(x2, y2, width2, height2);
-	}
+	ctx.strokeRect(x1, y1, width1, height1);
+	ctx.strokeRect(x2, y2, width2, height2);
 }
+
+$(document).ready (
+	function(){
+		draw();
+    }
+);
