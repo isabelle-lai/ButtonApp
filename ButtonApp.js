dfe7f9733804621b0189739ceb3aef8e52acaa84
isabelle-lai <55671247+isabelle-lai@users.noreply.github.com>
2019-11-04 14:06:31 -0500

    created js file

---
+++ ButtonApp.js
@@ -0,0 +1,15 @@
+var canvas = document.getElementById('myCanvas');
+var ctx = canvas.getContext('2d');
+var x1 = 50;
+var y1 = 50;
+var width1 = 100;
+var height1 = 100;
+var x2 = 150;
+var y2 = 150;
+var width1 = 100;
+var height1 = 100;
+
+function draw(){
+	ctx.strokeRect(x1, y1, width1, height1);
+	ctx.strokeRect(x2, y2, width2, height2);
+}
